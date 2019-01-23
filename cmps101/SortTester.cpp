55c086c0793e2e62cd34e1a174edcfc3fc20eddc
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-01-22 22:50:59 -0800

    Moved sorter test code from algorithms_and_ds/MergeSort to this project.

---
+++ cmps101/SortTester.cpp
@@ -0,0 +1,98 @@
+//
+// Created by alfredd on 1/22/19.
+//
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+
+void VerifySorted(FILE *unsortedFD, FILE *sortedFD);
+
+int GetIntegerArrayFromLine(char *sortedLine, int *temp);
+
+int CompareIntegerArrays(int *sa, int n1, int *usa, int n2);
+
+int CountOnes(int *array, int index);
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <TestCasesInput.txt> <SortedOutputFromInsertSortLinked.txt>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    FILE *unsortedFD = fopen(argv[1], "r");
+    FILE *sortedFD = fopen(argv[2], "r");
+    VerifySorted(unsortedFD, sortedFD);
+    fclose(unsortedFD);
+    fclose(sortedFD);
+}
+
+void VerifySorted(FILE *unsortedFD, FILE *sortedFD) {
+    int results[100000]={0}, resultIndex=0, out1, out2;
+    while (true) {
+        char *sortedLine, *unsortedLine;
+        size_t sortedLength, unsortedLength;
+        out1= getline(&sortedLine, &sortedLength, sortedFD);
+
+
+        int sortedArray[100000] = {0};
+        int n1 = GetIntegerArrayFromLine(sortedLine, sortedArray);
+        int *unsortedArray = (int*) malloc(sizeof(int) *n1);
+        bzero(unsortedArray,n1);
+        out2 = getline(&unsortedLine, &unsortedLength, unsortedFD);
+        int n2 = GetIntegerArrayFromLine(unsortedLine, unsortedArray);
+        if(n1!=n2) {
+            results[resultIndex]=0;
+        } else {
+            results[resultIndex]=CompareIntegerArrays(sortedArray, n1,unsortedArray, n2);
+        }
+        free(unsortedArray);
+        free(sortedLine);
+        free(unsortedLine);
+        if (out1==-1 || out2==-1)
+            break;
+        resultIndex+=1;
+    }
+    if(out2!= out1) {
+        printf("Mismatch in the number of lines in the input text files.\n");
+    }
+    printf("%d line(s) verified in the file. %d line(s) contain successfully sorted integers.\n", resultIndex, CountOnes(results, resultIndex));
+}
+
+int CountOnes(int *array, int index) {
+    int ones=0;
+    for (int i = 0; i < index; ++i) {
+        if(array[i] ==1 )
+            ++ones;
+    }
+    return ones;
+}
+
+int CompareIntegerArrays(int *sa, int n1, int *usa, int n2) {
+    vector<int> v1;
+    v1.assign(usa,usa+n2);
+    sort(v1.begin(), v1.end());
+    int status=1;
+    for(int i =0;i<n1;i++) {
+        if (v1.at(i) != sa[i]) {
+            status=0;
+            break;
+        }
+    }
+    return status;
+}
+
+int GetIntegerArrayFromLine(char *sortedLine, int *temp) {
+    int n = 0;
+    char *found = sortedLine;
+    while (found != NULL) {
+        sscanf(&sortedLine[found - sortedLine], "%d", &temp[n]);
+        found = strchr(&sortedLine[(found - sortedLine + 1)], ' ');
+        n += 1;
+    }
+    return  n;
+}
