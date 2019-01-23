6975eeb66acaa0dda6617a99c0e142ad16c2b150
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-01-22 23:39:34 -0800

    Script to allow cloning project repositories to local machine by deadline date.

--- cmps101/scripts/deadline_cloner.sh
+++ cmps101/scripts/deadline_cloner.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 argc=$#
+program_name=$0
+function usage_and_exit() {
 
-if [ "$argc" != "2" ] ;
-then
-    echo "Invalid usage. Cannot continue executing the program."
-    echo "usage: $0 cruzid_list.txt "DEADLINE DateString"
-    echo "      example"
-    echo "          $0 cruzid_list.txt "\"2019-01-01 23:59:59\""
-fi
+    if [ "$argc" != "3" ] ;
+    then
+        echo "Invalid usage. Cannot continue executing the program."
+        echo "usage: $program_name cruzid_list.txt "DEADLINE DateString"
+        echo "      example"
+        echo "          $program_name cruzid_list.txt "\"2019-01-01 23:59:59\""
+    fi
+}
+
+usage_and_exit
 
 filename=$1
 datestring=$2
+branch_name=$3
 git_directory="ucsc_gitlab"
 
 echo "Starting git clone-ing. Project repositories will be created under ./$git_directory"
 mkdir -p "$git_directory"
 
+if ! [[ -e "$filename" ]] ;
+then
+    echo "File does not exist.";
+    usage_and_exit ;
+fi
+
 for cruzid in `cat $filename`;
 do
     echo "Cloning repo for $cruzid" ;
-    git clone
+    git clone git@gitlab.soe.ucsc.edu:cmps101/winter19-01/$cruzid.git ./$git_directory/
+    cd $git_directory/$cruzid
+    git checkout -b $branch_name `git rev-list -1 --before="$datestring" master`
 done
