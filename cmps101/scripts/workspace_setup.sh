5aa2fca1c338b9c11a65b76e06e819408d002107
Abhishek Alfred Singh <929416+alfredd@users.noreply.github.com>
2019-01-27 17:09:31 -0800

    Correcting bugs in script.

--- cmps101/scripts/workspace_setup.sh
+++ cmps101/scripts/workspace_setup.sh
@@ -19,11 +19,10 @@ unzip master.zip  -d cmps101_scripts
 
 
 cd $workspace
-ln -s "$workspace/cmps101_scripts/ucsc_work-master/cmps101/scripts/deadline_cloner.sh" "$binaries/dc.sh"
 
 chmod +x "$workspace/cmps101_scripts/ucsc_work-master/cmps101/scripts/deadline_cloner.sh"
-
 ln -s "$workspace/cmps101_scripts/ucsc_work-master/cmps101/scripts/deadline_cloner.sh" "$binaries/dc.sh"
+
 
 chmod +x "$workspace/cmps101_scripts/ucsc_work-master/cmps101/scripts/sorttester.py"
 ln -s "$workspace/cmps101_scripts/ucsc_work-master/cmps101/scripts/sorttester.py" "$binaries/isltester"
